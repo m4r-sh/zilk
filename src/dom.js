0fa5d9b35576d01890781b7bb7900e60b75253f2
m4r-sh <me@m4r.sh>
2026-07-24 18:01:49 -0500

    fix browser-side strings

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,8 @@
 import { Hole, html, svg, htmlFor, svgFor, render } from 'uhtml/keyed'
 import { classify, fmt as _fmt, plain } from './_render-utils.js'
 
-const raw = new Proxy(plain,{ get: (_,k) => (...args) => new Hole(plain(...args)) })
-const fmt = new Proxy(_fmt,{ get: (_,k) => (...args) => new Hole(_fmt(...args)) })
+const raw = new Proxy(plain,{ get: (_,k) => (...args) => plain(...args) })
+const fmt = new Proxy(_fmt,{ get: (_,k) => (...args) => _fmt(...args) })
 const { css, md, glsl } = fmt
 
 export { 
