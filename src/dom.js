220091430c3823e59cf800d64f0f0551b93bfb4a
m4r-sh <me@m4r.sh>
2026-08-20 18:23:45 -0500

    update fetch handler

--- src/dom.js
+++ src/dom.js
@@ -3,9 +3,9 @@ import { classify, fmt as _fmt, plain } from './_render-utils.js'
 
 const raw = new Proxy(plain,{ get: (_,k) => (...args) => plain(...args) })
 const fmt = new Proxy(_fmt,{ get: (_,k) => (...args) => _fmt(...args) })
-const { css, md, glsl } = fmt
+const { css, md, glsl, wgsl } = fmt
 
 export { 
   html, svg, htmlFor, svgFor, render,
-  raw, fmt, css, md, glsl, classify, Hole
+  raw, fmt, css, md, glsl, wgsl, classify, Hole
 }
